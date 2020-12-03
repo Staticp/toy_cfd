a14b761bcfec4af16265e288ae4cb98e38daf52a
Staticp <364925857@qq.com>
2020-12-03 11:24:52 +0800

    courett flow

---
+++ simple/build/CMakeFiles/Couette.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/static/mycode/simple/src/main.cpp" "/home/static/mycode/simple/build/CMakeFiles/Couette.dir/src/main.cpp.o"
+  "/home/static/mycode/simple/src/simple.cpp" "/home/static/mycode/simple/build/CMakeFiles/Couette.dir/src/simple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simple/build/CMakeFiles/Couette.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Couette.dir/src/main.cpp.o"
+  "CMakeFiles/Couette.dir/src/simple.cpp.o"
+  "../bin/Couette.pdb"
+  "../bin/Couette"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Couette.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
